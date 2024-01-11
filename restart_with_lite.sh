d0647c5b9107d500de8c000797bcb4085d490fbd
pengxg <pengxianggui@outlook.com>
2024-01-11 10:51:25 +0800

    update restart_with_lite.sh

--- restart_with_lite.sh
+++ restart_with_lite.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-PID=$(ps -ef | grep "python3 startup.py -a --lite" | grep -v grep | awk '{print $2}')
-
-if [ -n "$PID" ]; then
-	echo "发现此脚本在运行中, 即将kill.."
-	kill $PID
-else
-	echo "未发现此脚本在运行中, 即将直接启动.."
-fi
-
+./shutdown_all.sh
 nohup python3 startup.py -a --lite > logs/restart_with_lite.output.log 2>&1 &
 echo "Python3已在后台运行, 进程ID为 $!"
 
