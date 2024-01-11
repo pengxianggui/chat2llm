b89b51089b09249dbb303e93d1269bea69bea5df
pengxg <pengxianggui@outlook.com>
2024-01-11 11:09:27 +0800

    更新脚本

--- restart_with_lite.sh
+++ restart_with_lite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +777 showdown_all.sh
+chmod a+x shutdown_all.sh
 ./shutdown_all.sh
 nohup python3 startup.py -a --lite > logs/restart_with_lite.output.log 2>&1 &
 echo "Python3已在后台运行, 进程ID为 $!"
