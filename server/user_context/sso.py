89fc6c5b729f6980f28e679349d8f8fe4c0951d9
pengxg <pengxianggui@marssenger.com>
2024-06-04 10:02:32 +0800

    sso支持自定义重定向地址，方便本地h5连接正式环境服务端

--- server/user_context/sso.py
+++ server/user_context/sso.py
@@ -15,25 +15,29 @@ from server.third.ent_wechat import get_ent_chat_user
 # 统一单点登录处理接口
 def sso_handler(client: Union[str, None] = Query(default="0", description="客户端id，必须是系统支持的，用以声明h5环境。默认是0表示浏览器环境"),
                 secret: Union[str, None] = Query(default=None, description="当client为0时必传"),
+                redirect_url: Union[str, None] = Query(default=None, description="当需要指定重定向时可传入。通常用于本地h5连接"),
                 request: Request = None):
+    if redirect_url is None:
+        redirect_url = H5_ADDRESS
+
     if client is None:
-        return RedirectResponse(url=f"{H5_ADDRESS}/401?message=未知客户端,无法单点登录,请重新进入。")
+        return RedirectResponse(url=f"{redirect_url}/401?message=未知客户端,无法单点登录,请重新进入。")
 
     if client == "0":  # h5演示页面
-        return redirect_h5_demo(secret, request)
+        return redirect_h5_demo(secret, redirect_url, request)
     if client == "1":  # 企微页面
         REDIRECT_URI = parse.quote(SERVER_ADDRESS + "/ent-wechat-sso")
         return RedirectResponse(url=f"https://open.weixin.qq.com/connect/oauth2/authorize?appid={ENT_WECHAT_CORPID}"
                                     f"&redirect_uri={REDIRECT_URI}&response_type=code&scope=snsapi_privateinfo"
                                     f"&state={client}&agentid={ENT_WECHAT_AGENTID}#wechat_redirect")
 
-    return RedirectResponse(url=f"{H5_ADDRESS}/401?message=无效的客户端,无法单点登录,请重新进入")
+    return RedirectResponse(url=f"{redirect_url}/401?message=无效的客户端,无法单点登录,请重新进入")
 
 
 # 重定向到h5 demo页面。
-def redirect_h5_demo(secret: str, request: Request):
+def redirect_h5_demo(secret: str, redirect_url: str, request: Request):
     if secret != H5_DEMO_SECRET:
-        return RedirectResponse(url=f"{H5_ADDRESS}/401?message=单点失败, 请提供正确口令")
+        return RedirectResponse(url=f"{redirect_url}/401?message=单点失败, 请提供正确口令")
 
     client_id = 0  # h5演示页面默认的client
     client = get_client(client_id)  # 获取默认的client
@@ -47,7 +51,7 @@ def redirect_h5_demo(secret: str, request: Request):
         user = add_user(client.id, ip, ip)
 
     token = encrypt(client.id, user.user_id, user.user_id)
-    redirect_to = f"{H5_ADDRESS}/sso?token={token}"
+    redirect_to = f"{redirect_url}/sso?token={token}"
     print(f"redirect to {redirect_to}")
     return RedirectResponse(url=redirect_to)
 
